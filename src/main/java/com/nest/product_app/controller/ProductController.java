c4de7e7575bcbc1168f40c1824eab0f27db9d3d5
Jitty-04 <jittysjohn1997@gmail.com>
2022-12-05 20:26:01 +0530

    edit

--- src/main/java/com/nest/product_app/controller/ProductController.java
+++ src/main/java/com/nest/product_app/controller/ProductController.java
@@ -7,20 +7,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
     @PostMapping("/")
-    public String Add(){
+    public String Home(){
+        return "welcome to home page";
+    }
+    @PostMapping("/productadd")
+    public String AddProduct(){
         return "welcome to add product page";
     }
-    @PostMapping("/search")
-    public String Search(){
+    @PostMapping("/searchproduct")
+    public String SearchProduct(){
         return "welcome to search product page";
     }
-    @GetMapping("/viewall")
-    public String View(){
+    @GetMapping("/viewallproduct")
+    public String ViewAll(){
         return "welcome to view all product page";
     }
-    @PostMapping("/delete")
-    public String Delete(){
+    @PostMapping("/deleteproduct")
+    public String DeleteProduct(){
         return "welcome to delete product page";
+    }
+    @PostMapping("/editproduct")
+    public String EditProduct(){
+        return "welcome to edit product page";
     }
 
 }
